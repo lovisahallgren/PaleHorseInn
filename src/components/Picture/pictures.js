732274d67ebe6b335c3da76a7644caffa3b30050
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-03-11 19:31:02 +0100

    moved bg-image and fonts to public folder

--- src/components/Picture/pictures.js
+++ src/components/Picture/pictures.js
@@ -1,62 +1,62 @@
 const pictures = [{
         name: 'Kebabpizza',
-        image: '/pictures/5).jpg',
+        image: '/images/5).jpg',
     },
     {
         name: 'Butterfly',
-        image: '/pictures/Butterfly.jpg',
+        image: '/images/Butterfly.jpg',
     },
     {
         name: 'Capricciosa',
-        image: '/pictures/ekollon.jpg',
+        image: '/images/ekollon.jpg',
     },
     {
         name: 'Honeybee',
-        image: '/pictures/Honey_bee_1.jpg',
+        image: '/images/Honey_bee_1.jpg',
     },
     {
         name: 'Margherita',
-        image: '/pictures/IMG_9241.jpg',
+        image: '/images/IMG_9241.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/IMG_9276.jpg',
+        image: '/images/IMG_9276.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Island_in_the_sun.jpg',
+        image: '/images/Island_in_the_sun.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/knot.jpg',
+        image: '/images/knot.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Main(0-00-07-02).jpg',
+        image: '/images/Main(0-00-07-02).jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/MIO4.jpg',
+        image: '/images/MIO4.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Part1.jpg',
+        image: '/images/Part1.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Part2.jpg',
+        image: '/images/Part2.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Thunder2.jpg',
+        image: '/images/Thunder2.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/Tomatoes.jpg',
+        image: '/images/Tomatoes.jpg',
     },
     {
         name: 'Quattro stagioni',
-        image: '/pictures/VikingShip.jpg',
+        image: '/images/VikingShip.jpg',
     },
 ];
 
