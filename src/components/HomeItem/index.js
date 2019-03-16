81ce57d619835b0a3dfa058ac2b454a2285b5f34
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-03-16 15:07:18 +0100

    fixed home page style for desktop

--- src/components/HomeItem/index.js
+++ src/components/HomeItem/index.js
@@ -6,10 +6,10 @@ import './HomeItem.css';
 const HomeItem = (props) => {
     return (
         <div className="main-content">
-        <div className="bg"></div>
-        <div className={props.id}>
-            <h3 className="homeTitle">{props.homeTitle}</h3>
-        </div>
+            <div className="bg"></div>
+            <div className={props.id}>
+                <h3 className="homeTitle">{props.homeTitle}</h3>
+            </div>
         </div>
     )
 }
